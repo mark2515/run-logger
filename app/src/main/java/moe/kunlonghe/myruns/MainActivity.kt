d15a4aaab655a2e824ba9e75e55ce47eb7819469
mark2515 <hekunlong2515@gmail.com>
2025-09-27 21:23:12 -0700

    Create the ActionBar and Tabs

--- app/src/main/java/moe/kunlonghe/myruns/MainActivity.kt
+++ app/src/main/java/moe/kunlonghe/myruns/MainActivity.kt
@@ -1,16 +1,57 @@
 package moe.kunlonghe.myruns
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragmentStart: FragmentStart
+    private lateinit var fragmentHistory: FragmentHistory
+    private lateinit var fragmentSettings: FragmentSettings
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var myMyFragmentStateAdapter: MyFragmentStateAdapter
+    private lateinit var fragments: ArrayList<Fragment>
+    private val tabTitles = arrayOf("Start", "History", "Settings") //Tab titles
+    private lateinit var tabConfigurationStrategy: TabConfigurationStrategy
+    private lateinit var tabLayoutMediator: TabLayoutMediator
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)        
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, UserProfileActivity::class.java)
-        startActivity(intent)
-        finish()
+        viewPager2 = findViewById(R.id.viewpager)
+        tabLayout = findViewById(R.id.tab)
+
+        fragmentStart = FragmentStart()
+        fragmentHistory = FragmentHistory()
+        fragmentSettings = FragmentSettings()
+
+        fragments = ArrayList()
+        fragments.add(fragmentStart)
+        fragments.add(fragmentHistory)
+        fragments.add(fragmentSettings)
+
+        myMyFragmentStateAdapter = MyFragmentStateAdapter(this, fragments)
+        viewPager2.adapter = myMyFragmentStateAdapter
+
+        tabConfigurationStrategy = TabConfigurationStrategy {
+                tab: TabLayout.Tab, position: Int ->
+            tab.text = tabTitles[position] }
+        tabLayoutMediator = TabLayoutMediator(tabLayout, viewPager2, tabConfigurationStrategy)
+        tabLayoutMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabLayoutMediator.detach()
     }
 }
